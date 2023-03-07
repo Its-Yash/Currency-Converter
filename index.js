5a4f02509e9d9ec39241d5e299a795c35e966242
Yash Pathak <90565364+Its-Yash@users.noreply.github.com>
2023-03-07 23:35:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const currencyFirstEl = document.getElementById("currency-first")
+
+const worthFirstEl = document.getElementById("worth-first")
+
+const currencySecondEl = document.getElementById("currency-second")
+
+const worthSecondEl = document.getElementById("worth-second")
+
+const exchangeRateEl = document.getElementById("exchange-rate")
+
+updateRate ()
+
+function updateRate(params) {
+    fetch(`https://v6.exchangerate-api.com/v6/a927790fc1a6476141105e94/latest/${currencyFirstEl.value}`).then((res)=>res.json()).then((data)=>
+        {
+            const rate = data.conversion_rates[currencySecondEl.value]
+            console.log(rate);
+            exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${ rate + " " + currencySecondEl.value}`
+
+            worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2)
+        })
+    
+}
+
+
+currencyFirstEl.addEventListener("change", updateRate)
+
+currencySecondEl.addEventListener("change", updateRate)
+
+worthFirstEl.addEventListener("input", updateRate)
+
